b1d9c357f91a8594a93ee73e96bef5d4a4e9f467
minecraft0036 <aosi90736@gmail.com>
2026-02-21 18:45:01 +0900

    s

--- CSarpSorce/aesw/obj/Release/net10.0/aesw.AssemblyInfo.cs
+++ CSarpSorce/aesw/obj/Release/net10.0/aesw.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("File String Encrypt")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("aesw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ed44947e02efed8607ed6ed678bcf895dde08c")]
-[assembly: System.Reflection.AssemblyProductAttribute("File String Encrypt")]
-[assembly: System.Reflection.AssemblyTitleAttribute("File String Encrypt")]
+[assembly: System.Reflection.AssemblyProductAttribute("aesw")]
+[assembly: System.Reflection.AssemblyTitleAttribute("aesw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment クラスによって生成されました。
