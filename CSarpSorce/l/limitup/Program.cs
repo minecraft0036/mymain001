1efa2034cba3a1e7a2fcad1783d234c460200090
minecraft0036 <aosi90736@gmail.com>
2026-03-09 18:01:23 +0900

    a

---
+++ CSarpSorce/l/limitup/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace limitup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void AnimateWriteLine(string text, ConsoleColor color, int duration)
+        {
+
+        }
+    }
+}
