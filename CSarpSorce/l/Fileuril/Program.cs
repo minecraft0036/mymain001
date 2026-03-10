9afa59353a201c5a13ca0773512601c0cdd17a9f
minecraft0036 <aosi90736@gmail.com>
2026-03-10 21:22:45 +0900

    1 day ago

---
+++ CSarpSorce/l/Fileuril/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Fileuril
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            WriteTitle("Extractor",ConsoleColor.DarkGreen,ConsoleColor.DarkRed);Console.Title = "extractor";Console.CursorVisible = false;
+            AnimateEriteLine("minecraft", ConsoleColor.Green, 80);
+        }
+        static void WriteTitle(string name,ConsoleColor color,ConsoleColor textcolor)
+        {
+            var oldcolor = Console.BackgroundColor;
+            var textold = Console.ForegroundColor;
+            Console.ForegroundColor = textcolor;
+            Console.BackgroundColor=color;
+            Console.WriteLine(name);
+            Console.ForegroundColor = textold;
+            Console.BackgroundColor = oldcolor;
+        }
+        static void AnimateEriteLine(string text,ConsoleColor color,int duration)
+        {
+            var textold = Console.ForegroundColor;
+            Console.ForegroundColor=color;
+            foreach (var item in text)
+            {
+                Console.Write(item);
+                Thread.Sleep(duration);
+            }
+            Console.Write("\n");
+            Console.ForegroundColor = textold;
+        }
+    }
+}
