f6fcb52c7f7c121507a4e55e1b61e2a7a8b96b2d
minecraft0036 <aosi90736@gmail.com>
2026-03-11 15:13:00 +0900

    k

--- CSarpSorce/l/Fileuril/Program.cs
+++ CSarpSorce/l/Fileuril/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Fileuril
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace Fileuril
 {
     internal class Program
     {
@@ -8,32 +12,49 @@
         }
         static void Main(string[] args)
         {
-            
-            WriteTitle("Extractor",ConsoleColor.DarkGreen,ConsoleColor.DarkRed);Console.Title = "extractor";Console.CursorVisible = false;
-            AnimateEriteLine("minecraft", ConsoleColor.Blue, 80);
-            Console.Title = "Extractor";
 
-        }
-        static void WriteTitle(string name,ConsoleColor color,ConsoleColor textcolor)
-        {
-            var oldcolor = Console.BackgroundColor;
-            var textold = Console.ForegroundColor;
-            Console.ForegroundColor = textcolor;
-            Console.BackgroundColor=color;
-            Console.WriteLine(name);
-            Console.ForegroundColor = textold;
-            Console.BackgroundColor = oldcolor;
-        }
-        static void AnimateEriteLine(string text,ConsoleColor color,int duration)
-        {
-            var textold = Console.ForegroundColor;
-            Console.ForegroundColor=color;
-            foreach (var item in text)
+            Thread.GetDomain().SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
+            var pri = (WindowsPrincipal)Thread.CurrentPrincipal;
+
+            //管理者権限以外での起動なら, 別プロセスで本アプリを起動する
+            if (!pri.IsInRole(WindowsBuiltInRole.Administrator))
             {
-                Console.Write(item);
-                Thread.Sleep(duration);
+                var proc = new ProcessStartInfo()
+                {
+                    FileName=Environment.ProcessPath,
+                    Verb = "RunAs"
+                };
+                Process.Start(proc);
+                return;
+
+
+
+
             }
-            Console.ForegroundColor = textold;
+            Console.WriteLine("Hello!");
+            Console.ReadLine();
         }
+            static void WriteTitle(string name, ConsoleColor color, ConsoleColor textcolor)
+            {
+                var oldcolor = Console.BackgroundColor;
+                var textold = Console.ForegroundColor;
+                Console.ForegroundColor = textcolor;
+                Console.BackgroundColor = color;
+                Console.WriteLine(name);
+                Console.ForegroundColor = textold;
+                Console.BackgroundColor = oldcolor;
+            }
+            static void AnimateEriteLine(string text, ConsoleColor color, int duration)
+            {
+                var textold = Console.ForegroundColor;
+                Console.ForegroundColor = color;
+                foreach (var item in text)
+                {
+                    Console.Write(item);
+                    Thread.Sleep(duration);
+                }
+                Console.ForegroundColor = textold;
+            }
+        
     }
 }
